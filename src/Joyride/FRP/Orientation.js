eb4a1a7f076d6749cd00c93bcb441c7ec0a364ea
Mike Solomon <mike120982@gmail.com>
2022-06-13 13:57:01 +0300

    Asks for permission

---
+++ src/Joyride/FRP/Orientation.js
@@ -0,0 +1,17 @@
+export const permission = () => {
+	if (
+		typeof DeviceMotionEvent !== "undefined" &&
+		typeof DeviceMotionEvent.requestPermission === "function"
+	) {
+		// (optional) Do something before API request prompt.
+		return DeviceMotionEvent.requestPermission().then((response) => {
+			// (optional) Do something after API prompt dismissed.
+			if (response == "granted") {
+				return true;
+			}
+			return false;
+		});
+	} else {
+		return Promise.resolve(true);
+	}
+};
