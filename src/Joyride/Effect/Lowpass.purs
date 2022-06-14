2713c93eeca48baf5d402f0e03b817b569b4c18f
Mike Solomon <mike120982@gmail.com>
2022-06-14 12:23:20 +0300

    Adds lpf

---
+++ src/Joyride/Effect/Lowpass.purs
@@ -0,0 +1,17 @@
+module Joyride.Effect.Lowpass where
+
+import Prelude
+
+import Data.Maybe (Maybe(..))
+import Data.Tuple.Nested ((/\))
+import FRP.Event (Event, mapAccum)
+
+lpf :: Number -> Event Number -> Event Number
+lpf lowpassFactor = flip
+  ( mapAccum
+      ( \a b -> Just a /\ case b of
+          Nothing -> a
+          Just x -> (x * lowpassFactor) + (a * (1.0 - lowpassFactor))
+      )
+  )
+  Nothing
