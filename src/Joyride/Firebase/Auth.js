63f1f0df625c89a002c7480f1a46bb79e1ed3af4
Mike Solomon <mike120982@gmail.com>
2022-06-24 15:30:26 +0300

    Fixes auto anon

--- src/Joyride/Firebase/Auth.js
+++ src/Joyride/Firebase/Auth.js
@@ -1,28 +1,21 @@
 export const firebaseAuth = (app) => () =>
 	import("firebase/auth").then(({ getAuth }) => getAuth(app));
 
-export const onAuthStateChanged =
-	(errorF) => (signedIn) => (auth) => () =>
-		import("firebase/auth").then(
-			({ onAuthStateChanged, signInAnonymously }) => {
-				const thunk = onAuthStateChanged(auth, (user) => {
-					if (user) {
-						signedIn(user)();
-					} else {
-						if (autoAnon) {
-							signInAnonymously(auth)
-								.then(() => {
-									// Signed in..
-									// do nothing
-								})
-								.catch((error) => {
-									errorF(error)();
-								});
-						} else {
-							signedOut();
-						}
-					}
-				});
-				return thunk;
+export const onAuthStateChanged = (errorF) => (signedIn) => (auth) => () =>
+	import("firebase/auth").then(({ onAuthStateChanged, signInAnonymously }) => {
+		const thunk = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				signedIn(user)();
+			} else {
+				signInAnonymously(auth)
+					.then(() => {
+						// Signed in..
+						// do nothing
+					})
+					.catch((error) => {
+						errorF(error)();
+					});
 			}
-		);
+		});
+		return thunk;
+	});
