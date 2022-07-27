bd502d7d6fae03ba888821986b7afb0c8229cf1d
Mike Solomon <mike120982@gmail.com>
2022-07-27 16:58:12 +0300

    Emits source maps

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
 	mode: "development",
 	entry: "./src/index.js",
+	devtool: "source-map",
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js",
