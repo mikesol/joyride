f22c0207d1a4fca180f1bad2c345596b875cdbf0
Mike Solomon <mike120982@gmail.com>
2022-09-06 16:36:48 +0300

    Removes min/max check for gyroscope ambitus

--- src/Joyride/FRP/Orientation.purs
+++ src/Joyride/FRP/Orientation.purs
@@ -23,7 +23,7 @@ foreign import orientationPermission :: Effect (Promise Boolean)
 posFromOrientation :: GTP -> JMilliseconds -> Number
 posFromOrientation gtp (JMilliseconds curMs) = case gtp.time of
   Nothing -> 0.0
-  Just (JMilliseconds prevMs) -> min 1.0 $ max (-1.0) $ ((curMs - prevMs) / 1000.0) * gtp.gamma * orientationDampening + gtp.pos
+  Just (JMilliseconds prevMs) -> ((curMs - prevMs) / 1000.0) * gtp.gamma * orientationDampening + gtp.pos
   where
   orientationDampening = 0.055 :: Number
 
