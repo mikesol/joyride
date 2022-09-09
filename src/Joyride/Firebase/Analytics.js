f2947bbed16a949f94a69c6cdc0259596e524ece
Mike Solomon <mike120982@gmail.com>
2022-09-09 12:24:59 +0300

    Fixes fb

--- src/Joyride/Firebase/Analytics.js
+++ src/Joyride/Firebase/Analytics.js
@@ -1,4 +1,4 @@
 export const firebaseAnalytics = (app) => () =>
 	import("firebase/analytics").then(({ getAnalytics }) =>
-		getAnalytics(import.meta.env.NODE_ENV === "production" ? app : undefined)
+		getAnalytics(import.meta.env.BUILD_TYPE === "production" ? app : undefined)
 	);
