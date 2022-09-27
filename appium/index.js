36127776b6b902bcd4b58bf7f93e251a759c77e0
Mike Solomon <mike120982@gmail.com>
2022-09-27 11:19:52 +0300

    Adds more instructions to test

--- appium/index.js
+++ appium/index.js
@@ -7,17 +7,32 @@ const opts = {
 };
 
 async function main() {
-    const client = await wdio.remote(opts);
+    const browser = await wdio.remote(opts);
 
     //const field = await client.$("android.widget.EditText");
     //await field.setValue("Hello World!");
     //const value = await field.getText();
     //assert.strictEqual(value,"Hello World!");
-    await browser.url('https://joyride.fm')
-    const apiLink = await browser.$('=API')
-    await apiLink.click()
-
-    await client.deleteSession();
+    await browser.url('https://joyride.fm?stats=true')
+    await browser.pause(3000)
+    const tutorialButton = await browser.$('#tutorialButton')
+    await tutorialButton.click()
+    const startTutorial = await browser.$('#startTutorial')
+    await startTutorial.click()
+    await browser.pause(2000)
+    const nextEarnPoints = await browser.$('#nextEarnPoints')
+    await nextEarnPoints.click()
+    await browser.pause(10000)
+    const nextTilt = await browser.$('#nextTilt')
+    await nextTilt.click()
+    await browser.pause(10000)
+    const nextLeap = await browser.$('#nextLeap')
+    await nextLeap.click()
+    await browser.pause(10000)
+    const nextLong = await browser.$('#nextLong')
+    await nextLong.click()
+    await browser.pause(5000)
+    await browser.deleteSession();
 }
 
 main();
